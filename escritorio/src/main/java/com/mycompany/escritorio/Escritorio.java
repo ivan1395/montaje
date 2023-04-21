178fe270876923b199ce917c99060250bf64d719
Iván <102693294+ivan1395@users.noreply.github.com>
2023-04-21 14:37:45 +0200

    Add files via upload

---
+++ escritorio/src/main/java/com/mycompany/escritorio/Escritorio.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.escritorio;
+
+/**
+ *
+ * @author Curso Tarde
+ */
+public class Escritorio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        NewJFrame marco=new NewJFrame();
+        marco.setVisible(true);
+    }
+}
